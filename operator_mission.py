7751dd680f041249eff6fb4ad820f3abb1241748
codeo1io <codeolio@protonmail.com>
2026-09-22 03:23:43 +0000

    fix(lint): drop unused op_workspace import in operator_mission

--- operator_mission.py
+++ operator_mission.py
@@ -45,7 +45,6 @@ import operator_policy as op
 import operator_diagnostics as op_diag
 import operator_cron as op_cron
 import operator_fleet as op_fleet
-import operator_workspace as op_workspace
 
 # ---------------------------------------------------------------------------
 # Constants
